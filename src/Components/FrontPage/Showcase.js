f6a08914089a645b104c3685f94bc1fcc96ac8a7
felcg <xfelipecg@gmail.com>
2020-06-02 17:02:34 -0300

    Modifica texto e css da secao showcase

--- src/Components/FrontPage/Showcase.js
+++ src/Components/FrontPage/Showcase.js
@@ -1,35 +1,51 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import {
+  Container, Row, Col, Image,
+} from 'react-bootstrap'
+import LandingImage from '../../Assets/img/undraw_image_viewer_wxce.svg'
+import ObjetividadeImage from '../../Assets/img/undraw_online_connection_6778.svg'
+import ConversaoImage from '../../Assets/img/undraw_revenue_3osh.svg'
+
 
 const Showcase = () => (
-  <section className="showcase">
+  <section className="showcase bg-light">
     <Container fluid className="p-0">
-      <Row noGutters>
-        <Col lg={{ span: 6, order: 1 }} className="text-white showcase-img" id="showcase-image-1" />
+      <Row className="showcase-row">
+        <Col lg={{ span: 6, order: 1 }} className="showcase-col-img">
+          <Image src={LandingImage} className="showcase-img mx-auto my-auto" />
+        </Col>
         <Col lg={{ span: 6, order: 2 }} className="showcase-text my-auto">
-          <h2>Design Responsivo</h2>
+          <h2 className="text-primary">O que é Landing Page?</h2>
           <p className="lead mb-0">
-            Ao usar esse tema você pode ter a certeza que vai ficar ótimo em qualquer aparelho.
-            A página vai ser responsiva no celular, tablet ou computador.
+            A landing page oferece ao visitante todas a informação necessária para que ele
+            conheça e deseje o produto. É muito mais seguro que ele finalize a compra. Você
+            está em uma landing page!
           </p>
         </Col>
       </Row>
-      <Row noGutters>
-        <Col lg={{ span: 6, order: 2 }} className="text-white showcase-img" id="showcase-image-2" />
+      <Row className="showcase-row">
+        <Col lg={{ span: 6, order: 2 }} className="showcase-col-img">
+          <Image src={ObjetividadeImage} className="showcase-img mx-auto my-auto" />
+        </Col>
         <Col lg={{ span: 6, order: 1 }} className="showcase-text my-auto">
-          <h2>Pronta para Bootstrap 4</h2>
+          <h2 className="text-primary">Objetividade</h2>
           <p className="lead mb-0">
-            Bootstrap 4 está liderando o caminho para o desenvolvimento responsivo.
-            Todos os nossos temas estão usando essa framework!
+            As informações presentes em uma landing page são mais objetivas,
+            indo direto ao ponto e passando com clareza o que o cliente precisa saber
+            e o que ele vai ganhar preenchendo o formulário daquela página.
           </p>
         </Col>
       </Row>
-      <Row noGutters>
-        <Col lg={{ span: 6, order: 1 }} className="text-white showcase-img" id="showcase-image-3" />
+      <Row className="showcase-row">
+        <Col lg={{ span: 6, order: 1 }} className="showcase-col-img">
+          <Image src={ConversaoImage} className="showcase-img mx-auto my-auto" />
+        </Col>
         <Col lg={{ span: 6, order: 2 }} className="showcase-text my-auto">
-          <h2>Fácil de usar e customizar</h2>
+          <h2 className="text-primary">Conversão</h2>
           <p className="lead mb-0">
-            Adicione seu conteúdo e imagens e sua landing page já vai estar pronta para o mercado!
+            Quanto menos distrações e opções de clique a página tiver,
+            maiores são as chances de o usuário realizar a ação que está determinada ali.
+            E aí, a taxa de conversão sobe significativamente.
           </p>
         </Col>
       </Row>
